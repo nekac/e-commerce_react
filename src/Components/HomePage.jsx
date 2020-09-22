fe0cc93eb22b10c99fb6d7f4343d015680436d8d
nekac <nikola.nenin91@gmail.com>
2020-09-22 15:28:07 +0200

    Dir structure for components and styles + new files added

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "../Styles/homepage.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="menu">
+      <div className="item">
+        <div className="content">
+          <h1 className="title">CATEGORY 1</h1>
+          <span className="subtitle"> Some subtitle 1</span>
+        </div>
+      </div>
+      <div className="item">
+        <div className="content">
+          <h1 className="title">CATEGORY 2</h1>
+          <span className="subtitle"> Some subtitle 2</span>
+        </div>
+      </div>
+      <div className="item">
+        <div className="content">
+          <h1 className="title">CATEGORY 3</h1>
+          <span className="subtitle"> Some subtitle 3</span>
+        </div>
+      </div>
+      <div className="item">
+        <div className="content">
+          <h1 className="title">CATEGORY 4</h1>
+          <span className="subtitle"> Some subtitle 4</span>
+        </div>
+      </div>
+      <div className="item">
+        <div className="content">
+          <h1 className="title">CATEGORY 5</h1>
+          <span className="subtitle"> Some subtitle 5</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
